607e1a22788d121ea60a48989387566109848e3f
jdom1824 <jdom1824@gmail.com>
2025-06-17 17:51:38 -0500

    d

--- build/lib/framework_bt/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Framework-BT  –  API pública
-Solo re-exportamos la función extract para que pueda usarse así:
-    from framework_bt import extract
-"""
-from .extractor import extract
-
-__all__ = ["extract"]
-__version__ = "0.0.1"
